d25146e31a2ce18680d0832c2d7a80e7320f1ccf
didac-velasco <Didac2003velasco>
2023-02-21 16:02:05 +0100

    canvi de pag a concerts + nou layout preferencies

---
+++ app/src/main/java/com/example/artistesmusicals/Concerts.java
@@ -0,0 +1,18 @@
+package com.example.artistesmusicals;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+public class Concerts extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.concerts);
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.tigini);
+        mediaPlayer.start();
+
+    }
+
+}
--- app/src/main/java/com/example/artistesmusicals/MainActivity.java
+++ app/src/main/java/com/example/artistesmusicals/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.artistesmusicals;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,8 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.tigini);
-        mediaPlayer.start();
 
 
         Button btnLocalitatas = (Button) findViewById(R.id.btnLocalitzacions);
@@ -25,13 +22,17 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(myIntent);
             }
         });
-    }
-    /*public void btnView(View v){
-        Intent intent = new Intent(MainActivity.this, Concerts.class);
-        startActivity(intent);
+
+        Button btnConcerts = (Button) findViewById(R.id.btnConcerts);
+        btnConcerts.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent2 = new Intent (v.getContext(), Concerts.class);
+                startActivity(myIntent2);
+            }
+        });
     }
 
-    public void btnViewResoltes(View v){
+    /*public void btnViewResoltes(View v){
         Intent intent = new Intent(MainActivity.this, LlistaIncidenciesResoltes.class);
         startActivity(intent);
     }*/
