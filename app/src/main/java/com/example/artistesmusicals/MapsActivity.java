07264d8375640f2ce2ab4ff1d37c2c6fa477ee02
GuillemBSK <gmagrina1@institutjaumehuguet.cat>
2023-02-26 19:31:14 +0100

    Maps funcional.

--- app/src/main/java/com/example/artistesmusicals/MapsActivity.java
+++ app/src/main/java/com/example/artistesmusicals/MapsActivity.java
@@ -14,16 +14,15 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
     }
 
     /**
@@ -31,12 +30,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
 
         LatLng institut = new LatLng(41.289558967903304, 1.2459889876953898);   //Posicio de l'institut
-        mMap.addMarker(new MarkerOptions().position(institut).title("Jaume Huguet").icon(BitmapDescriptorFactory.fromResource(R.drawable.pin))); //Personalitzacio del marcador
-        mMap.moveCamera(CameraUpdateFactory.zoomTo(17.0f)); //Fixem el zoom
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(institut));
-        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE); //Tipus de mapa
+        googleMap.addMarker(new MarkerOptions().position(institut).title("Jaume Huguet").icon(BitmapDescriptorFactory.fromResource(R.drawable.pin))); //Personalitzacio del marcador
+        googleMap.moveCamera(CameraUpdateFactory.zoomTo(17.0f)); //Fixem el zoom
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(institut));
+        googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE); //Tipus de mapa
     }
 }
