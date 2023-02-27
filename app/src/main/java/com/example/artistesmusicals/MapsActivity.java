55b18239b488324dfe9b3e920a180ca8f3556faf
GuillemBSK <gmagrina1@institutjaumehuguet.cat>
2023-02-27 23:38:42 +0100

    Ubicacions afegides.

--- app/src/main/java/com/example/artistesmusicals/MapsActivity.java
+++ app/src/main/java/com/example/artistesmusicals/MapsActivity.java
@@ -31,10 +31,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
-        LatLng institut = new LatLng(41.289558967903304, 1.2459889876953898);   //Posicio de l'institut
-        googleMap.addMarker(new MarkerOptions().position(institut).title("Jaume Huguet").icon(BitmapDescriptorFactory.fromResource(R.drawable.pin))); //Personalitzacio del marcador
-        googleMap.moveCamera(CameraUpdateFactory.zoomTo(17.0f)); //Fixem el zoom
+        LatLng institut = new LatLng(41.289558967903304, 1.2459889876953898);
+        googleMap.addMarker(new MarkerOptions().position(institut).title("Jaume Huguet").icon(BitmapDescriptorFactory.fromResource(R.drawable.pin)));
+
+        LatLng madrid = new LatLng(40.4167047, -3.7035825);
+        googleMap.addMarker(new MarkerOptions().position(madrid).title("Madrid").icon(BitmapDescriptorFactory.fromResource(R.drawable.pin)));
+
+        LatLng barcelona = new LatLng(41.3828939, 2.1774322);
+        googleMap.addMarker(new MarkerOptions().position(barcelona).title("Barcelona").icon(BitmapDescriptorFactory.fromResource(R.drawable.pin)));
+
+        LatLng paris = new LatLng(48.8588897, 2.320041);
+        googleMap.addMarker(new MarkerOptions().position(paris).title("Paris").icon(BitmapDescriptorFactory.fromResource(R.drawable.pin)));
+
+        LatLng berlin = new LatLng(52.5170365, 13.3888599);
+        googleMap.addMarker(new MarkerOptions().position(berlin).title("Berlin").icon(BitmapDescriptorFactory.fromResource(R.drawable.pin)));
+
+        googleMap.moveCamera(CameraUpdateFactory.zoomTo(5.0f)); //Fixem el zoom
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(institut));
-        googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE); //Tipus de mapa
+        googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID); //Tipus de mapa
+
     }
 }
